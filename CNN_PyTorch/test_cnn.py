c6731ec5b32a9d0cce7434152f4a64bfd5b4f353
Kartikaya-git <ph1230668@physics.iitd.ac.in>
2025-11-01 18:49:47 +0530

    Added the confidence parameter

--- CNN_PyTorch/test_cnn.py
+++ CNN_PyTorch/test_cnn.py
@@ -2,14 +2,17 @@ import torch
 from torchvision import datasets, transforms
 from model_cnn_def import CNN
 from PIL import Image
+import cv2
+import numpy as np
+
 
 # 1. Load your trained model
 model = CNN()  
-model.load_state_dict(torch.load("mnist_cnn.pth",weights_only=True, map_location=torch.device('cpu')))
+model.load_state_dict(torch.load("CNN_PyTorch/mnist_cnn.pth",weights_only=True, map_location=torch.device('cpu')))
 model.eval()
 
 # 2. Load your image
-img_path = "/Users/kartikayasrivastava/Desktop/Digit_24oct/digits/two.jpg"   # <-- change this to your file name
+img_path = "/Users/kartikayasrivastava/Desktop/Digit_24oct/digits/nine.jpg"   # <-- change this to your file name
 image = Image.open(img_path) 
 
 transform = transforms.Compose([
@@ -25,6 +28,8 @@ image = image.unsqueeze(0)  # add batch dimension: [1,1,28,28]
 # 4. Predict
 with torch.no_grad():
     output = model(image)
-    predicted = torch.argmax(output, dim=1).item()
+    probabilities = torch.softmax(output, dim=1)
+    confidence, predicted = torch.max(probabilities, 1)
 
-print(f"Predicted Digit: {predicted}")
+print(f"Predicted Digit: {predicted.item()}")
+print(f"Confidence: {confidence.item()*100:.2f}%")
